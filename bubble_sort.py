37c23aeedf1518e6e10d4f49e4cacda94642ef19
AshotArushanyan <61308373+AshotArushanyan@users.noreply.github.com>
2020-09-24 23:12:15 +0400

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -22,7 +22,9 @@ def bubble_sorting(list):
     for i in range(len(list)):
         for j in range(len(list)-1):
             if list[j]["published"] > list[j+1]["published"]:
-                list[j],list[j+1] = list[j+1],list[j]
+                temporary = list[j]
+                list[j] = list[j+1]
+                list[j+1] = temporary
     return list
 
 
